d5680bf78eec9895386cabb2ce8766f9559b1115
Luiz Henrique <Luiizhenrique97444@gmail.com>
2023-05-16 15:28:15 -0300

    login

---
+++ script/login.js/login.js
@@ -0,0 +1,71 @@
+const form = document.querySelector("form"),
+  emailField = form.querySelector(".email-field"),
+  emailInput = emailField.querySelector(".email"),
+  passField = form.querySelector(".create-password"),
+  passInput = passField.querySelector(".password"),
+  cPassField = form.querySelector(".confirm-password"),
+  cPassInput = cPassField.querySelector(".cPassword");
+
+// Validação de e-mail
+function checkEmail() {
+  const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (!emailInput.value.match(emaiPattern)) {
+    return emailField.classList.add("invalid");// adicionando classe inválida se o valor do e-mail não for compatível com o padrão do e-mail
+  }
+  emailField.classList.remove("invalid"); //// removendo classe inválida se o valor do e-mail corresponder ao emaiPattern
+}
+
+// Oculta e mostra a senha
+const eyeIcons = document.querySelectorAll(".show-hide");
+
+eyeIcons.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    const pInput = eyeIcon.parentElement.querySelector("input"); //obtendo o elemento pai do ícone do olho e selecionando a entrada de senha
+    if (pInput.type === "password") {
+      eyeIcon.classList.replace("bx-hide", "bx-show");
+      return (pInput.type = "text");
+    }
+    eyeIcon.classList.replace("bx-show", "bx-hide");
+    pInput.type = "password";
+  });
+});
+
+// Validação de senha
+function createPass() {
+  const passPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (!passInput.value.match(passPattern)) {
+    return passField.classList.add("invalid"); // adicionando classe inválida se o valor de entrada da senha não corresponder a passPattern
+  }
+  passField.classList.remove("invalid"); // removendo classe inválida se o valor de entrada da senha corresponder a passPattern
+}
+
+// Confirma a validação da senha
+function confirmPass() {
+  if (passInput.value !== cPassInput.value || cPassInput.value === "") {
+    return cPassField.classList.add("invalid");
+  }
+  cPassField.classList.remove("invalid");
+}
+
+// Chamando função no Form Sumbit
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //impedindo o envio do formulário
+  checkEmail();
+  createPass();
+  confirmPass();
+
+ //chamando função ao teclar
+  emailInput.addEventListener("keyup", checkEmail);
+  passInput.addEventListener("keyup", createPass);
+  cPassInput.addEventListener("keyup", confirmPass);
+
+  if (
+    !emailField.classList.contains("invalid") &&
+    !passField.classList.contains("invalid") &&
+    !cPassField.classList.contains("invalid")
+  ) {
+    location.href = form.getAttribute("action");
+  }
+});
