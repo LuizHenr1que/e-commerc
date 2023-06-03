71b11dca62ea70167acd4d4ca1797d0511f48cd1
Luiz Henrique <Luiizhenrique97444@gmail.com>
2023-06-03 18:19:12 -0300

    att

--- script/slider-info.js
+++
@@ -1,9 +0,0 @@
-const lista = document.querySelector(".lista-info")
-
-const slider = (e) => {
-    lista.scrollLeft = e.pageX;
-}
-
-if (window.innerWidth === 450) {
-    lista.addEventListener("mousemove", slider); 
-}
